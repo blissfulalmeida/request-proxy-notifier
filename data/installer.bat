f9a71b6aae0f96d4b1654bfbba29d99efa728400
danylo <danylo.maryskevych@gmail.com>
2024-10-13 18:28:59 +0300

    update bat

--- data/installer.bat
+++ data/installer.bat
@@ -1,15 +1,29 @@
 @echo off
-echo Setting PowerShell execution policy...
-PowerShell -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
 
-echo Installing Scoop...
-PowerShell -Command "iex '& {$(irm get.scoop.sh)} -RunAsAdmin'"
+REM Download NirCmd zip file
+powershell -Command "Invoke-WebRequest -Uri 'https://www.nirsoft.net/utils/nircmd.zip' -OutFile 'nircmd.zip'"
 
-echo Installing Python using Scoop...
-scoop install python
+REM Extract NirCmd zip file to C:\Program Files\nircmd
+powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::ExtractToDirectory('nircmd.zip', 'C:\Program Files\nircmd')"
 
-echo Installing Pipx using Scoop...
-scoop install pipx
+REM Set execution policy to RemoteSigned
+powershell -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
 
-echo Installation complete.
+REM Install Scoop and Python
+powershell -Command "iex '& {$(irm get.scoop.sh)} -RunAsAdmin'"
+
+REM Install Python and Pipx
+powershell -Command "scoop install python"
+powershell -Command "scoop install pipx"
+powershell -Command "pipx ensurepath"
+powershell -Command "pipx install mitmproxy"
+powershell -Command "pipx inject mitmproxy httpx"
+
+REM Install Git
+powershell -Command "scoop install git"
+
+REM Clone the GitHub repository
+powershell -Command "git clone https://github.com/blissfulalmeida/request-proxy-notifier.git"
+
+echo All commands executed successfully!
 pause
